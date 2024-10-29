867995ba6fe7175a21bc1c999d1bf29a838a914e
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-29 17:53:19 -0400

    added countdownNumber function

--- main.ts
+++ main.ts
@@ -6,3 +6,11 @@
 */
 
 basic.clearScreen()
+let strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+let countdownNumber = 0
+
+countdownNumber = 5
+input.onButtonPressed(Button.A, function() {
+    while (countdownNumber > 0)
+    basic.showNumber(countdownNumber)
+})
