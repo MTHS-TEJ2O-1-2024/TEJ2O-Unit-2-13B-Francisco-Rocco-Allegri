a20f3253d17a196590ab7e05f7e055dd38b6cb73
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-11-01 20:49:09 -0400

    finished countdown + neopixel code

--- main.ts
+++ main.ts
@@ -2,15 +2,31 @@
  *
  * Created by: Francisco Rocco Allegri
  * Created on: Oct 2024
- * This program ...
+ * This program makes a countdown from 4 to 0, and by each value of number, the number of neopixels lighting up
 */
 
 basic.clearScreen()
-let strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
-let countdownNumber = 0
+let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
-countdownNumber = 5
-input.onButtonPressed(Button.A, function() {
-    while (countdownNumber > 0)
-    basic.showNumber(countdownNumber)
+input.onButtonPressed(Button.A, function () {
+    let countdownNumber: number = 4
+
+    while (countdownNumber >= 0) {
+        basic.showNumber(countdownNumber)
+        neopixelStrip.clear()
+
+        if (countdownNumber > 0) neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        if (countdownNumber > 1) neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        if (countdownNumber > 2) neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        if (countdownNumber > 3) neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+
+        neopixelStrip.show()
+
+        basic.pause(1000)
+        countdownNumber -= 1
+    }
+
+    basic.clearScreen()
+    neopixelStrip.clear()
+    neopixelStrip.show()
 })
